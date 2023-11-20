fe1d0573b22fe30ab67676a92359cb2c6fbcf2c8
Yakov Damen <yakov.damen@gmail.com>
2023-11-20 14:26:30 +0200

    {Add} shop inventory route

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,6 +5,7 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import initProductTable from './configs/db.js';
 import { notFound, errorHandler } from './middlewares/errorsMiddleware.js';
+import { shop_inventoryRouter } from './routes/shopInventoryRouts.js';
 
 const app = express();
 
@@ -15,9 +16,9 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use('/api/shop_inventory', shop_inventoryRouter);
 app.use(notFound);
 app.use(errorHandler);
-app.use('/api/shop_inventory', )
 
 const port = process.env.PORT || 5000;
 
