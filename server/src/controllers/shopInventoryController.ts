510a70f1c893b58040f0e61e36f81f250f699d85
Yakov Damen <yakov.damen@gmail.com>
2023-11-20 22:09:49 +0200

    {Refactor} shopInventoryController.ts

--- server/src/controllers/shopInventoryController.ts
+++ server/src/controllers/shopInventoryController.ts
@@ -1,35 +1,25 @@
-import { Request, Response } from 'express'
-import * as Service from '../services/shopInventoryRouterService'
+import { Request, Response } from 'express';
+import * as Service from '../services/shopInventoryService';
+import asyncHandler from 'express-async-handler';
 
-export const externalGetAllData = async (req: Request, res: Response) => {
-    try {
-        const myUrl = req.url;
-        const searchParam =  req.query.search?.toString();
-        const allData =  await Service.getAllData(searchParam);
-        res.send(allData)
-    }
-    catch (error) {
-        console.error('an error occurred:', error);
-        res.status(500).send("Internal Server Error");
-    }
-}
+export const getAllData = asyncHandler(async (req: Request, res: Response) => {
+  const searchParam = req.query.search?.toString();
+  const allData = await Service.getAllData(searchParam);
+  res.json(allData);
+});
 
-export const getProductById = async (req: Request, res: Response) => {
-  try {
+export const getProductById = asyncHandler(
+  async (req: Request, res: Response) => {
+    const productId = Number(req.params.product_id);
+    // const productId = 12;
 
-    
-    // const productId = Number(req.params.product_id);
-    const productId = 12
-    if (isNaN(productId) || productId <= 0) {
-      return res.status(400).json({ message: "Invalid product ID format" });
-    }
     const data = await Service.getProductById(productId);
-    if (data === null || data === undefined) {
-      return res.status(404).json({ message: "Product not found" });
-    }
-    res.status(200).json(data);
-  } catch (error) {
-    console.error("Error in getting a single product", error);
-    return res.status(500).json({ error: "Internal Server Error" });
+
+    res.json(data);
   }
+);
+
+export const updateInventory = async (req: Request, res: Response) => {
+  const success = await Service.updateInventory(req);
+  res.json(success);
 };
