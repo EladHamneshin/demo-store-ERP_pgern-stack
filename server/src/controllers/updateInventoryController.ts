9df4bba49308e89c2240f6b0080da0fbb210cd30
israel-lissak <islissak@gmail.com>
2023-11-20 15:19:14 +0200

    added updateInventory endpoint

---
+++ server/src/controllers/updateInventoryController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+import { updateInventoryService } from '../services/updateInventoryService';
+
+export async function updateInventoryController(req: Request, res: Response) {
+    try {
+        console.log("controller");
+        
+        // ניסיון לעדכן את המלאי
+        const success = await updateInventoryService(req);
+        res.status(200).json(success)
+    } catch (error) {
+        // טיפול בשגיאה במקרה של חריגה
+        console.error(error);
+        return res.status(500).send('Internal Server Error');
+    }
+}
--- server/src/routes/shopInventoryRouts.ts
+++ server/src/routes/shopInventoryRouts.ts
@@ -1,7 +1,9 @@
 import express from 'express';
+import { updateInventoryController } from '../controllers/updateInventoryController';
 
 const shop_inventoryRouter = express.Router();
 
 
+shop_inventoryRouter.post('/updateInventory', updateInventoryController);
 
-
+export default shop_inventoryRouter
