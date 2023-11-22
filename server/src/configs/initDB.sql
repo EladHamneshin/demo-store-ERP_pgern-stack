441b44a588744ecfb43ebd8a7a3c3d08adc9093f
Yakov Damen <yakov.damen@gmail.com>
2023-11-22 09:05:33 +0200

    Update database schema and add sample data

--- server/src/configs/initDB.sql
+++ server/src/configs/initDB.sql
@@ -1,33 +1,41 @@
+-- Active: 1694601384132@@127.0.0.1@5432@demo-store
+
+-- Active: 1694601384132@@127.0.0.1@5432@demo-store
+
+-- Active: 1694601384132@@127.0.0.1@5432@demo-store
+
+-- Active: 1694601384132@@127.0.0.1@5432@demo-store
+
 CREATE TABLE
     IF NOT EXISTS coordinates (
-        id UUID PRIMARY KEY,
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
         lat FLOAT NOT NULL,
         lng FLOAT NOT NULL
     );
 
 CREATE TABLE
     IF NOT EXISTS categories (
-        id UUID PRIMARY KEY,
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
         name VARCHAR(255) NOT NULL,
         clicked INT NOT NULL
     );
 
 CREATE TABLE
     IF NOT EXISTS images (
-        id UUID PRIMARY KEY,
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
         url VARCHAR(255) NOT NULL,
         alt VARCHAR(255) NOT NULL
     );
 
 CREATE TABLE
     IF NOT EXISTS products (
-        id UUID PRIMARY KEY,
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
         name VARCHAR(21) NOT NULL,
         price INT NOT NULL,
         quantity INT NOT NULL,
         description VARCHAR(255) NOT NULL,
-        image INT NOT NULL,
-        category INT NOT NULL,
+        image UUID NOT NULL,
+        category UUID NOT NULL,
         discount INT NOT NULL,
         rating INT NOT NULL,
         clicked INT NOT NULL,
@@ -36,36 +44,35 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    IF NOT EXISTS productcoordinates (
-        id UUID PRIMARY KEY,
-        product INT NOT NULL,
-        coordinates INT NOT NULL,
+    IF NOT EXISTS product_coordinates (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        product UUID NOT NULL,
+        coordinates UUID NOT NULL,
         FOREIGN KEY (product) REFERENCES products(id),
         FOREIGN KEY (coordinates) REFERENCES coordinates(id)
     );
 
 CREATE TABLE
     IF NOT EXISTS tags (
-        id UUID PRIMARY KEY,
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
         name VARCHAR(255) NOT NULL
     );
 
 CREATE TABLE
-    IF NOT EXISTS tagvalues (
-        id UUID PRIMARY KEY,
+    IF NOT EXISTS tag_values (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
         name VARCHAR(255) NOT NULL,
-        tag INT NOT NULL,
+        tag UUID NOT NULL,
         FOREIGN KEY (tag) REFERENCES tags(id)
     );
 
 CREATE TABLE
-    IF NOT EXISTS producttags (
-        id UUID PRIMARY KEY,
-        product INT NOT NULL,
-        tag INT NOT NULL,
-        tagvalue INT NOT NULL,
+    IF NOT EXISTS product_tags (
+        id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+        product UUID NOT NULL,
+        tag_and_value_id UUID NOT NULL,
         FOREIGN KEY (product) REFERENCES products(id),
-        FOREIGN KEY (tag) REFERENCES tags(id)
+        FOREIGN KEY (tag_and_value_id) REFERENCES tag_values(id)
     );
 
 INSERT INTO coordinates (lat, lng) values (40.7128, -74.0060);
@@ -102,9 +109,17 @@ VALUES (
         'Samsung Galaxy S21',
         1000,
         540,
-        'Samsung Galaxy S21 5G | Factory Unlocked Android Cell Phone | US Version 5G Smartphone | Pro-Grade Camera, 8K Video, 64MP High Res | 128GB, Phantom Gray (SM-G991UZAAXAA)',
-        1,
-        1,
+        'Samsung Galaxy S21 5G | Factory Unlocked Android Cell Phone | US Version 5G Smartphone | Pro-Grade Camera, 8K Video, 64MP High Res | 128GB, Phantom Gray (SM-G991UZAAXAA)', (
+            SELECT id
+            FROM images
+            WHERE
+                alt = 'phone'
+        ), (
+            SELECT id
+            FROM categories
+            WHERE
+                name = 'Phones'
+        ),
         0,
         5,
         0
@@ -112,22 +127,213 @@ VALUES (
         'Lenovo IdeaPad 3',
         500,
         800,
-        'Lenovo IdeaPad 3 14 Laptop, 14.0 FHD 1920 x 1080 Display, AMD Ryzen 5 3500U Processor, 8GB DDR4 RAM, 256GB SSD, AMD Radeon Vega 8 Graphics, Narrow Bezel, Windows 10, 81W0003QUS, Abyss Blue',
-        2,
-        2,
+        'Lenovo IdeaPad 3 14 Laptop, 14.0 FHD 1920 x 1080 Display, AMD Ryzen 5 3500U Processor, 8GB DDR4 RAM, 256GB SSD, AMD Radeon Vega 8 Graphics, Narrow Bezel, Windows 10, 81W0003QUS, Abyss Blue', (
+            SELECT id
+            FROM images
+            WHERE
+                alt = 'computer'
+        ), (
+            SELECT id
+            FROM categories
+            WHERE
+                name = 'Computers'
+        ),
         0,
         5,
         0
     );
-INSERT INTO
-    tagvalues (name, tag)
-VALUES ('Samsung', 1), ('Lenovo', 1), ('Plastic', 2), ('Metal', 2), ('Black', 3), ('White', 3), ('128GB', 4), ('256GB', 4);
 
 INSERT INTO
-    producttags 
-    (product, tag, tagvalue)
-VALUES (1, 1, 1), (2, 1, 2), (1, 2, 5), (2, 2, 4), (1, 3, 6), (2, 3, 5), (1, 4, 7), (2, 4, 8);
+    tag_values (name, tag)
+VALUES (
+        'Samsung', (
+            SELECT id
+            FROM tags
+            WHERE
+                name = 'brand'
+        )
+    ), (
+        'Lenovo', (
+            SELECT id
+            FROM tags
+            WHERE
+                name = 'brand'
+        )
+    ), (
+        'Plastic', (
+            SELECT id
+            FROM tags
+            WHERE
+                name = 'material'
+        )
+    ), (
+        'Metal', (
+            SELECT id
+            FROM tags
+            WHERE
+                name = 'material'
+        )
+    ), (
+        'Black', (
+            SELECT id
+            FROM tags
+            WHERE
+                name = 'color'
+        )
+    ), (
+        'White', (
+            SELECT id
+            FROM tags
+            WHERE
+                name = 'color'
+        )
+    ), (
+        '128GB', (
+            SELECT id
+            FROM tags
+            WHERE
+                name = 'storage'
+        )
+    ), (
+        '256GB', (
+            SELECT id
+            FROM tags
+            WHERE
+                name = 'storage'
+        )
+    );
 
 INSERT INTO
-    productcoordinates (product, coordinates)
-VALUES (1, 1), (2, 1);
+    product_tags (product, tag_and_value_id)
+VALUES ( (
+            SELECT id
+            FROM products
+            WHERE
+                name = 'Samsung Galaxy S21'
+        ), (
+            SELECT id
+            FROM tag_values
+            WHERE
+                name = 'Samsung'
+        )
+    ), ( (
+            SELECT id
+            FROM products
+            WHERE
+                name = 'Samsung Galaxy S21'
+        ), (
+            SELECT id
+            FROM tag_values
+            WHERE
+                name = 'Plastic'
+        )
+    ), ( (
+            SELECT id
+            FROM products
+            WHERE
+                name = 'Lenovo IdeaPad 3'
+        ), (
+            SELECT id
+            FROM tag_values
+            WHERE
+                name = 'Lenovo'
+        )
+    ), ( (
+            SELECT id
+            FROM products
+            WHERE
+                name = 'Lenovo IdeaPad 3'
+        ), (
+            SELECT id
+            FROM tag_values
+            WHERE
+                name = 'Metal'
+        )
+    ), ( (
+            SELECT id
+            FROM products
+            WHERE
+                name = 'Lenovo IdeaPad 3'
+        ), (
+            SELECT id
+            FROM tag_values
+            WHERE
+                name = 'Black'
+        )
+    ), ( (
+            SELECT id
+            FROM products
+            WHERE
+                name = 'Samsung Galaxy S21'
+        ), (
+            SELECT id
+            FROM tag_values
+            WHERE
+                name = 'White'
+        )
+    ), ( (
+            SELECT id
+            FROM products
+            WHERE
+                name = 'Samsung Galaxy S21'
+        ), (
+            SELECT id
+            FROM tag_values
+            WHERE
+                name = '128GB'
+        )
+    ), ( (
+            SELECT id
+            FROM products
+            WHERE
+                name = 'Lenovo IdeaPad 3'
+        ), (
+            SELECT id
+            FROM tag_values
+            WHERE
+                name = '256GB'
+        )
+    );
+
+INSERT INTO
+    product_coordinates (product, coordinates)
+VALUES ( (
+            SELECT id
+            FROM products
+            WHERE
+                name = 'Lenovo IdeaPad 3'
+        ), (
+            SELECT id
+            FROM coordinates
+            WHERE
+                lat = 40.7128
+        )
+    ), ( (
+            SELECT id
+            FROM products
+            WHERE
+                name = 'Samsung Galaxy S21'
+        ), (
+            SELECT id
+            FROM coordinates
+            WHERE
+                lat = 40.7128
+        )
+    );
+
+
+DROP TABLE coordinates;
+
+DROP TABLE categories;
+
+DROP TABLE images;
+
+DROP TABLE products;
+
+DROP TABLE product_coordinates;
+
+DROP TABLE tags;
+
+DROP TABLE tag_values;
+
+DROP TABLE product_tags;
