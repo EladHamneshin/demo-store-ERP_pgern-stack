4961705c7f57dc9e0f78c0719a42d2837d60d148
Yakov Damen <yakov.damen@gmail.com>
2023-11-23 12:14:05 +0200

    Add react-router-dom and update routing configuration

--- client/src/routes/Router.tsx
+++ client/src/routes/Router.tsx
@@ -1,6 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import App from '../App';
 import ROUTES from './routes';
+import HomePage from '../pages/HomePage';
+import LoginPage from '../pages/LoginPage';
+import ProductPage from '../pages/ProductPage';
+import RegisterPage from '../pages/RegisterPage';
 
 const Router = () => {
   return (
@@ -11,7 +15,7 @@ const Router = () => {
         <Route path={ROUTES.REGISTER} element={<RegisterPage />} />
         <Route path={ROUTES.PRODUCT} element={<ProductPage />} />
       </Route>
-      <Route path={ROUTES.DEFAULT} element={<NotFoundPage />} />
+      {/* <Route path={ROUTES.DEFAULT} element={<NotFoundPage />} /> */}
     </Routes>
   );
 };
