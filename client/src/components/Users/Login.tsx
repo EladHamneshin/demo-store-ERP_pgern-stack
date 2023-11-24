9557cbbc48c9a8ec7461bf501b6db63f272f9e18
DvirBitan123 <dvirbitan123455@gmail.com>
2023-11-24 14:55:22 +0200

    almost done signIn

--- client/src/components/Users/Login.tsx
+++ client/src/components/Users/Login.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useForm } from "react-hook-form";
 import {useState} from 'react';
 import { useNavigate } from "react-router-dom";
 import { useAppDispatch } from '../../utils/store/hooks';
@@ -15,97 +15,102 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Copyright } from '../Copyright';
-import { isValidEmail, isValidPassword } from '../validateFuncs';
-import { toastError } from '../toastUtils/toastUtils';
+import { emailValidet, passwordValidet } from "../validateFuncs";
 
-// TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
-export default function Login() {
-  const [userEmail, setUserEmail] = useState('');
+export default function Login2Test() {
+  const [Email, setEmail] = useState('');
+  const [myLink, setMyLink] = useState('');
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    const formEmail = data.get('email');
-    const password = data.get('password');
+  const { register, handleSubmit, formState: {errors, isValid} } = useForm();
+  const onSubmit = (data) => {
+      const email = data.email; 
+      const password = data.password;
+           
+      // const {id, email} = loginUser(formEmail, password) // => waiting for the function
+      // setEmail(email);
+      // let myLink = errors.password&&errors.email ? '' : '/'; 
+    };
 
-    // if (formEmail !== null) {
-    //   if (!isValidEmail(formEmail.toString())) {
-    //     toastError("Email must be a valid email");
-    //     // return
-    //   }
-    // }  // toastCheck
-
-    // const {id, email} = loginUser(formEmail, password) // => waiting for the function
-    // setUserEmail(email);
-    // let myLink = userEmail === formEmail ? '/' : ''; 
-  };
-
+  
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
+    <>
+     <ThemeProvider theme={defaultTheme}>
+     <Container component="main" maxWidth="xs">
         <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              // onChange={isValidPassword()}
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={() => {
-                dispatch(saveEmail(userEmail));
-                navigate('/')
+            <Box
+              sx={{
+                  marginTop: 8,
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
               }}
             >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item>
-                <Link href='/register' variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
-  );
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+                Sign in
+            </Typography>
+              <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 1 }}>
+                <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                autoComplete="email"
+                autoFocus
+                {...register("email", emailValidet)}
+                helperText ={errors.email?.message?.toString()}
+                error={errors.email ? true : false}
+                />
+                <TextField
+                margin="normal"
+                required
+                fullWidth
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                {...register("password", passwordValidet)}
+                helperText={errors.password?.message?.toString()}
+                error={errors.password ? true : false}
+                />
+                <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={() => {
+                    dispatch(saveEmail(Email));
+
+                    console.log('all:', errors);
+                    console.log('email error:',errors.email?.message);
+                    console.log('password error:',errors.password?.message);
+                    if (errors.password && errors.email) setMyLink('');
+                    else if (!errors.password && !errors.email) setMyLink('/');
+                    console.log('link:',myLink);
+                    navigate(myLink)
+                }}
+                // disabled={!isValid}
+                >
+                Sign In
+                </Button>
+                <Grid container>
+                    <Grid item>
+                        <Link href='/register' variant="body2">
+                        {"Don't have an account? Sign Up"}
+                        </Link>
+                    </Grid>
+                </Grid>
+              </Box>    
+            </Box>
+            <Copyright sx={{ mt: 8, mb: 4 }} />
+        </Container>
+     </ThemeProvider>
+    </>
+  )
 }
