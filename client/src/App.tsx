246469c3bb28a73502e6badfab47fef003e4ae85
Yakov Damen <yakov.damen@gmail.com>
2023-11-26 09:18:15 +0200

    Add AppBar component to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
+import AppBar from './components/AppBar'
 
 function App() {
 
   return (
     <>
+      <AppBar/>
       <Outlet />  
     </>
   )
