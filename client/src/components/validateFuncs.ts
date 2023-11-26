648fa8f0d8ce7ebe78b7cdad1d4bb063de37f777
DvirBitan123 <dvirbitan123455@gmail.com>
2023-11-26 11:41:36 +0200

    login page +

--- client/src/components/validateFuncs.ts
+++ client/src/components/validateFuncs.ts
@@ -1,38 +1,12 @@
-export const passwordValidet = {
-    required: "⚠ Required field",
-    pattern: {
-      value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,20}$/,
-      message:
-        "⚠ Password must contain an uppercase letter, a lowercase letter, a digit, a special character, and be 8-20 characters long.",
-    },
-  };
-  export const emailValidet = {
-    required: "⚠ Required field",
-    pattern: {
-      value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-      message: "⚠ Invalid email address",
-    },
-  };
-  export const nameValidet = {
-    required: "⚠ Required field",
-    minLength: {
-      value: 2,
-      message: "⚠ Must be at least two characters",
-    },
-  };
+function isValidEmail(email: string) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
 
-// function isValidEmail(email: string) {
-//     return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-// }
+function isValidPassword(password: string) {
+  return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=.{7,20}$)/.test(password);
+}
 
-// function isValidPassword(password: string) {
-//     return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=.{7,20}$)/.test(password);
-// }
+export { isValidEmail, isValidPassword };
 
-// function isValidName(name: string){
-//     return name !== '';
-// }
-
-// export { isValidEmail, isValidPassword, isValidName };
 
 
