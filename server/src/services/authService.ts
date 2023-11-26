9f933ef32d996cbd75d2eaa7ef59b2555dc483e5
Yakov Damen <yakov.damen@gmail.com>
2023-11-26 16:45:44 +0200

    Fix error handling in authUser function

--- server/src/services/authService.ts
+++ server/src/services/authService.ts
@@ -7,10 +7,10 @@ const authUser = async (email: string, password: string) => {
     const user = await userDal.getUserByEmail(email);
     
     if (!user)
-        throw new RequestError('Invalid email', STATUS_CODES.UNAUTHORIZED);
+        throw new RequestError('User not registered', STATUS_CODES.UNAUTHORIZED);
 
-    // if (!await comparePassword(password, user.password))
-    //     throw new RequestError('Invalid password', STATUS_CODES.UNAUTHORIZED);
+    if (!await comparePassword(password, user.password))
+        throw new RequestError('Invalid password', STATUS_CODES.UNAUTHORIZED);
 
     return user;
 }
