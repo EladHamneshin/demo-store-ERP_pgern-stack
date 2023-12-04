f796f57279ceaf47cf438c88e47e284196184a9b
israel-lissak <islissak@gmail.com>
2023-12-04 15:36:30 +0200

    changed to pool 1.0.1

--- server/src/utils/queryDB.ts
+++ server/src/utils/queryDB.ts
@@ -4,12 +4,7 @@ import STATUS_CODES from './StatusCodes';
 import { DatabaseError, PoolClient, Pool } from 'pg';
 
 const query = async (query: string) => {
-  // const pool: Pool = new Pool({
-  //   max: 20,
-  //   connectionTimeoutMillis: 2000,
-  //   idleTimeoutMillis: 30000
-  // });
-  // if (!pool) throw new RequestError('could not connect to the database', STATUS_CODES.INTERNAL_SERVER_ERROR)
+  
   try {
     const client: PoolClient = await connectDB()
     const res = await client.query(query);
