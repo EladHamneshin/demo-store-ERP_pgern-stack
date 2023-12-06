18bca3516488413235d77d306bc6ee0f8eea7bcf
israel-lissak <islissak@gmail.com>
2023-12-06 20:19:19 +0200

    changed user dal to sql functions

--- server/src/dal/userDal.ts
+++ server/src/dal/userDal.ts
@@ -3,11 +3,7 @@ import User from '../types/User';
 
 export const addUser = async (user: User) => {
   const { email, password } = user;
-  const insert = await query(`
-  insert into users (email, password)
-  values('${email}', '${password}')
-  RETURNING *
-  `);
+  const insert = await query(`SELECT * FROM add_user('${email}', '${password}');`);
   const result = insert?.rows[0];
   console.log(insert);
 
@@ -16,16 +12,12 @@ export const addUser = async (user: User) => {
 
 export const getUser = async (userId: string) => {
   const getUser = await query(`
-  select * from users
-  where id = '${userId}'
+  SELECT * FROM get_user('${userId}'::UUID);
   `);
   return getUser?.rows[0];
 };
 
 export const getUserByEmail = async (email: string) => {
-  const getUser = await query(`
-  select * from users
-  where email = '${email}'
-  `);
+  const getUser = await query(`SELECT * FROM get_user_by_email('${email}');`);
   return getUser?.rows[0];
 };
